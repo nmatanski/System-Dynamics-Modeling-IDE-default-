82f1062aaebe95b7693cc27de9651148deb56f40
Nikolai <Nikolai@website.com>
2018-01-11 23:58:43 +0200

    v1.0.0.0 (Initial)

---
+++ SimpleDrawing/SimpleDrawing/IDrawable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace SimpleDrawing
+{
+    public interface IDrawable
+    {
+        void Draw(object sender, Graphics g);
+    }
+}
